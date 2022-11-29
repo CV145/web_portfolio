0fcdf690e3101e0aadac57d6196c38ad359d4ab9
Carlos <carlosvaleriano45@yahoo.com>
2022-11-28 20:51:46 -0600

    Added custom stacked card carousel code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:stacked_card_carousel/stacked_card_carousel.dart';
+import 'package:web_portfolio/stacked_card_carousel.dart';
 
 void main() => runApp(const MyApp());
 

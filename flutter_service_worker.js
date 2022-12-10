f3d56eee43744b37141742dc2407064c595075ca
Carlos <carlosvaleriano45@yahoo.com>
2022-12-10 11:01:24 -0600

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,13 +3,14 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "92822cd0830b9264c71001eab94ffe26",
+  "assets/AssetManifest.json": "574bb14ce30e31810382190834acffd6",
 "assets/assets/images/rogue3.png": "6728a10028ed4e623038d1b3e4445fe4",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
 "assets/NOTICES": "a875b28d702fdd6eed1a7f4f4c7d8970",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/shaders/ink_sparkle.frag": "02b0db84cc4cba03b511948af9ab136d",
+"assets/web/images/rogue3.png": "6728a10028ed4e623038d1b3e4445fe4",
 "canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",
 "canvaskit/canvaskit.wasm": "bf50631470eb967688cca13ee181af62",
 "canvaskit/profiling/canvaskit.js": "38164e5a72bdad0faa4ce740c9b8e564",
@@ -25,8 +26,8 @@ const RESOURCES = {
 "images/rogue1.png": "e4f719b78476e8a858fc03b76d0ed128",
 "images/rogue2.png": "cfb89d10bb83446f5ca33da9ac55577b",
 "images/rogue3.png": "6728a10028ed4e623038d1b3e4445fe4",
-"index.html": "6a468416806e0c13a51f0f30dc816dca",
-"/": "6a468416806e0c13a51f0f30dc816dca",
+"index.html": "11cc192c15a793e0da9542b19fb2791a",
+"/": "11cc192c15a793e0da9542b19fb2791a",
 "main.dart.js": "bb529f6548d4ffcea4841223be1ad452",
 "manifest.json": "3aa6fe2febd0dd507de23d91e45f4766",
 "version.json": "bd341ba74d38c530da636604a427deda"
